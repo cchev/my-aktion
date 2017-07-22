71889af7bf56597957f6d1301640b582d53bf6b2
cchev <mail@cchev.de>
2017-07-22 18:48:18 +0200

    Revert pom changes

--- src/main/java/de/ritter/myaktion/controller/AktionEditController.java
+++ src/main/java/de/ritter/myaktion/controller/AktionEditController.java
@@ -20,7 +20,7 @@ import de.ritter.myaktion.util.Events.Updated;
  *
  */
 @Named 
-@SessionScoped
+@SessionScoped 
 public class AktionEditController implements Serializable {
 	private static final long serialVersionUID = -2035530493556059089L;
 	
