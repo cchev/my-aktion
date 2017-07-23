4360776e3a0740a44b686aacb0bbe02316e1c9e1
cchev <mail@cchev.de>
2017-07-23 12:22:41 +0200

    Added database connection for testcase

--- src/test/java/de/ritter/myaktion/test/AktionEditTest.java
+++ src/test/java/de/ritter/myaktion/test/AktionEditTest.java
@@ -4,6 +4,8 @@
 package de.ritter.myaktion.test;
 
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -18,9 +20,14 @@ import org.junit.runner.RunWith;
 
 import de.ritter.myaktion.controller.AktionEditController;
 import de.ritter.myaktion.controller.AktionEditController.Mode;
+import de.ritter.myaktion.data.AktionListProducer;
 import de.ritter.myaktion.model.Aktion;
 import de.ritter.myaktion.model.Konto;
 import de.ritter.myaktion.model.Spende;
+import de.ritter.myaktion.services.AktionService;
+import de.ritter.myaktion.services.AktionServiceBean;
+import de.ritter.myaktion.util.Events;
+import de.ritter.myaktion.util.Resources;
 
 /**
  * @author christian
@@ -29,20 +36,21 @@ import de.ritter.myaktion.model.Spende;
 @RunWith(Arquillian.class)
 public class AktionEditTest {
 	@Deployment
-	public static Archive<?> createTestArchive() {
-		//return ShrinkWrap.create(WebArchive.class, "test.war")
-			//	.addClasses(Aktion.class, Konto.class, Spende.class, AktionEditController.class)
-				//.addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-				//.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
-		//, AktionEditController.class
-		
+	public static Archive<?> createTestArchive() {		
 		return ShrinkWrap.create(WebArchive.class, "test.war")
-	            .addClasses(Aktion.class, Konto.class, Spende.class, AktionEditController.class)
-	            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+	            .addClasses(Aktion.class, Konto.class, Spende.class, AktionEditController.class,
+	            		AktionListProducer.class, AktionService.class, AktionServiceBean.class,
+	            		Resources.class, Events.class)
+	            .addAsResource("META-INF/test-persistence.xml", "META-INF/persistence.xml")
+	            .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+	            .addAsWebInfResource("test-ds.xml", "test-ds.xml");
 	}
 	
 	@Inject
 	AktionEditController aktionEditController;
+	
+	@Inject
+	AktionService aktionService;
 	
 	@Test 
 	public void testAddAktion() {
@@ -60,5 +68,7 @@ public class AktionEditTest {
 		   konto.setKontoNr("012345");
 		   konto.setNameDerBank("XXX Bank");
 		   aktionEditController.doSave();
+		   List<Aktion> aktionen = aktionService.getAllAktionen();
+		   Assert.assertEquals(aktion.getName(), aktionen.get(0).getName());
 	}
 }
